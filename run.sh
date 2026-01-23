919627043abf5aa9383c1e5738f1eded5a64f4d9
liruichen_wsl <anchor_lee@163.com>
2026-01-23 18:00:37 +0800

    upfate

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ IMAGE_NAME="gcov_test_repo:test"
 if [ -z "$(docker images -q $IMAGE_NAME 2> /dev/null)" ]; then
     ./build.sh $IMAGE_NAME
 fi
-docker rm -f gcov_test 2>/dev/null || true && docker run -d -e ECHO_APP_ID=ar0c/gcov_test --rm --name gcov_test --hostname ar0c/gcov_test-default $IMAGE_NAME /go/bin/main
+docker rm -f gcov_test 2>/dev/null || true && docker run -d -e ECHO_APP_ID=ar0c/gcov_test -p 8081:8081 --rm --name gcov_test --hostname ar0c/gcov_test-default $IMAGE_NAME /go/bin/main
